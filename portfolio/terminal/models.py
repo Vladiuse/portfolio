61e3ae36cd3d7c4a59aeab2417d57d6da661ede1
vladiuse <vladiuse@gmail.com>
2023-02-27 21:24:10 +0300

    remove mmpt model

--- portfolio/terminal/models.py
+++ portfolio/terminal/models.py
@@ -4,49 +4,4 @@ from django.core.files.storage import default_storage
 import os
 from django.template import Template, Context
 
-# Create your models here.
-class Directory(MPTTModel):
-    DIR = 'dir'
-    FILE = 'file'
-    FILES_PATH = 'files'
-    TYPES = (
-        (DIR, DIR),
-        (FILE, FILE)
-    )
-
-    name = models.CharField(max_length=50)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-    type = models.CharField(max_length=5, blank=True, default=DIR, choices=TYPES)
-    file_template = models.CharField(max_length=40, verbose_name='file_template', blank=True)
-
-
-    def file_path(self):
-        if self.type == self.FILE:
-            return self.file_template
-        else:
-            return ''
-
-    class MPTTMeta:
-        order_insertion_by = ['name']
-
-
-    def __str__(self):
-        return self.name
-
-    def get_content(self):
-        file_path = os.path.join(self.FILES_PATH, str(self.file_template))
-        text = default_storage.open(file_path).read()
-        template = Template(str(text))
-        context = Context({'user':'USER', 'x':'X VALUES'})
-        content = template.render(context)
-        return content
-
-
-
-class Poll(models.Model):
-    name = models.CharField(max_length=10, )
-
-class Image(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    img = models.ImageField(upload_to='images')
 
--- portfolio/terminal/views.py
+++ portfolio/terminal/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-# Create your views here.
-from .models import Directory, Poll
-from django.template.loader import get_template
-from django.template.context import RequestContext
 from django.template import Template, loader
 import time
-import os
-from django.conf import settings
 from .snipets_view import get_templates
-from pprint import pprint
+
 
 DEFAULT_COLOR_THEME = 'blue'
 AVAILABLE_COLOR_THEMES = [DEFAULT_COLOR_THEME, 'purple', 'green']
@@ -20,11 +14,9 @@ def index(request):
     color_theme = request.COOKIES.get('color_theme')
     if color_theme not in AVAILABLE_COLOR_THEMES:
         color_theme = DEFAULT_COLOR_THEME
-    nodes = Directory.objects.all()
     start_file_id = START_PAGE_FILE_ID
     DIRS = get_templates(to_json=False)
     content = {
-        'nodes': nodes,
         'start_file_id': start_file_id,
         'color_theme': color_theme,
         'DIRS': DIRS,
@@ -33,9 +25,7 @@ def index(request):
 
 
 def test(request):
-    poll = Poll.objects.get(pk=1)
     content = {
-        'poll': poll,
     }
     return render(request, 'terminal/test.html', content)
 
